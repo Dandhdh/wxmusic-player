4e57adc4f82099aa82f70dfde22681ea8d58c62a
dengyaoyuan <405507582@qq.com>
2019-01-12 16:12:06 +0800

    单曲搜索功能，及搜索框的部分完善

--- pages/search/search.js
+++ pages/search/search.js
@@ -11,22 +11,27 @@ Page({
     singers:[],     // 输入时查询到的歌手
     songs:[],       // 输入时查询的歌曲
     songsResult:[],  // confirm后查询的歌曲
-    totalNum:0   // 单曲查询结果的总数量
+    totalNum:0,   // 单曲查询结果的总数量
+    keyWord:''
   },
   onLoad: function () {
     this._getHotSearch()
     this.dealHistroySearch()
   },
   _getHotSearch: function () {
+    console.log('热搜')
     api.getHotSearch().then((res) => {
       let res1 = res.data.replace('hotSearchKeysmod_top_search(', '')
       let res2 = JSON.parse(res1.substring(0, res1.length - 1))
       if (res2.code === 0) {
         let hotArr = res2.data.hotkey
+        console.log(this.data.hotSearch)
         this.setData({
           hotSearch: hotArr.length > hotSearchNum ? hotArr.slice(0, hotSearchNum) : hotArr
         })
+        console.log(this.data.hotSearch)
       }
+      console.log('2')
     }).catch((err) => {
       console.log(err)
     })
@@ -34,19 +39,31 @@ Page({
   // 快速搜索（在搜索框数据期间进行的搜索）
   searchAction: function (event) {
     const keyWord = event.detail.value || event.currentTarget.dataset.txt
-    // const type = event.type   // 区分是按回车的搜索，还是输入过程中的自动搜索
-    api.search(keyWord).then((res) => {
-      let res1 = res.data.replace('SmartboxKeysCallbackmod_top_search3847(', '')
-      let res2 = JSON.parse(res1.substring(0, res1.length - 1))
-      this.dealData(res2.data)
-    }).catch((res) => {
-      console.log(res)
+    this.setData({
+      keyWord: keyWord || '',
+      songsResult:[]
     })
+    if(keyWord && keyWord!=''){
+      api.search(keyWord).then((res) => {
+        let res1 = res.data.replace('SmartboxKeysCallbackmod_top_search3847(', '')
+        let res2 = JSON.parse(res1.substring(0, res1.length - 1))
+        this.dealData(res2.data)
+      }).catch((res) => {
+        console.log(res)
+      })
+    }else{
+      this._getHotSearch()
+      this.dealHistroySearch()
+      this.setData({
+        result:false
+      })
+    }
+    
   },
 
   // 单曲详细搜索
   searchSongAction: function(event){
-    const keyWord = event.detail.value || event.currentTarget.dataset.txt
+    const keyWord = event.detail.value || event.currentTarget.dataset.txt || this.data.keyWord
     const _this = this
     api.searchSong(keyWord).then((res) => {
       // console.log(res.data)
@@ -97,7 +114,7 @@ Page({
       })
     }
   },
-  dealHistroySearch: function (keyWord,mid) {
+  dealHistroySearch: function (keyWord) {
     let histroy
     try {
       let local = wx.getStorageSync('histroySearch')
